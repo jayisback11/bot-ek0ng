cf10f7e716c003693f2c8f64b50b352e1822884c
Jay B. Crisostomo <jaycris2000@gmail.com>
2022-03-11 23:25:22 -0600

    added line to change bot name

--- src/bot.js
+++ src/bot.js
@@ -11,13 +11,17 @@ const client = new Client({
 // When the client is ready, run this code (only once)
 client.on("ready", () => {
   console.log(`${client.user.username} has logged in!`);
+  bot.user.setUsername("BOT ek0ng");
 });
 
 client.on("messageCreate", (message) => {
-  if(Math.floor(Math.random() * 50) === 1){
-    message.reply("la ako paki")
-  } else if(message.author.username === "ek0ng" && Math.floor(Math.random() * 5) === 1){
-    message.reply("sml")
+  if (Math.floor(Math.random() * 50) === 1) {
+    message.reply("la ako paki");
+  } else if (
+    message.author.username === "ek0ng" &&
+    Math.floor(Math.random() * 5) === 1
+  ) {
+    message.reply("sml");
   }
 });
 
