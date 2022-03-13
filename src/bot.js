f29186bca6a73eea299430b3053c0383306721be
jc <jc>
2022-03-12 23:32:07 -0600

    adding pick command

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-
-// interacts with discord API
-const { Client, Intents } = require("discord.js");
+import dotenv from "dotenv";
+import { Client, Intents } from "discord.js";
+import { rollNumber } from "./roll.js";
+dotenv.config();
 
 // client is a instance of Client class
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, "GUILD_MESSAGES"]
+  intents: [Intents.FLAGS.GUILDS, "GUILD_MESSAGES"],
 });
 
 const PREFIX = "$";
@@ -18,39 +18,47 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
-
-  console.log(`${message.author.username}: ${message.content}`);
-  // if (Math.floor(Math.random() * 50) === 1) {
-  //   if(Math.floor(Math.random() * 1) === 0){
-  //     message.reply("la ako paki");
-  //   } else {
-  //     message.reply("isa ka nalang");
-  //   }
-  // } else
-  if (
-    message.author.username === "INCOGNITOJJ" &&
-    Math.floor(Math.random() * 5) === 1
-  ) {
-    message.reply("sml");
-  }
+  const indexOfPhrase = Math.floor(Math.random() * 2);
 
   // COMMANDS
-  else if (message.content.startsWith(PREFIX)) {
+  if (message.content.startsWith(PREFIX)) {
     const [CMD_NAME, ...args] = message.content
       .trim()
       .substring(PREFIX.length)
       .split(/\s+/);
 
     if (CMD_NAME === "roll") {
-      if (args.length === 0 || args.length === 1)
-        return message.reply('Please provide a range. e.g "$roll 1 3"');
-      else {
-        min = Math.ceil(args[0]);
-        max = Math.floor(args[1]);
-        message.reply(
-          `Rolled: ${Math.floor(Math.random() * (max - min) + min)}`
-        );
-      }
+      rollNumber(message, args);
+      return;
+    } else if (CMD_NAME === "pick") {
+    }
+  }
+
+  // MESSAGES
+  else if (
+    message.author.username === "INCOGNITOJJ" &&
+    Math.floor(Math.random() * 10) === 1
+  ) {
+    if (indexOfPhrase === 0) {
+      message.reply("sml");
+    } else {
+      message.reply("wehh");
+    }
+  } else if (
+    message.author.username === "Darren" &&
+    Math.floor(Math.random() * 10) === 1
+  ) {
+    if (indexOfPhrase === 0) {
+      message.reply("chill lang");
+    } else {
+      message.reply("nakoww... gumagalaw nanaman");
+    }
+  } else if (
+    message.author.username === "ek0ng" &&
+    Math.floor(Math.random() * 2) === 1
+  ) {
+    if (indexOfPhrase === 0) {
+      message.reply("wassup boss");
     }
   }
 });
---
+++ src/roll.js
@@ -0,0 +1,11 @@
+function rollNumber(message, args) {
+  if (args.length === 0 || args.length === 1)
+    return message.reply('Please provide a range. e.g "$roll 1 3"');
+  else {
+    const min = Math.ceil(args[0]);
+    const max = Math.floor(args[1]);
+    message.reply(`🎲 ${Math.floor(Math.random() * (max - min) + min)}`);
+  }
+}
+
+export { rollNumber };
