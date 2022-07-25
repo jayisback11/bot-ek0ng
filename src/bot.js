12eb7d476ec1a79657e2a258e37d3ae1c74c5dc8
Jay Crisostomo <jaycris2000@gmail.com>
2022-07-24 19:21:22 -0500

    added response for jabami

--- src/bot.js
+++ src/bot.js
@@ -1,44 +1,49 @@
-import dotenv from "dotenv";
-import { Client, Intents, ClientPresence, VoiceChannel } from "discord.js";
-import { rollNumber } from "./commands/roll.js";
-import { pickUser } from "./commands/pick.js";
-import { coinflip } from "./commands/coinflip.js";
-dotenv.config();
+import dotenv from 'dotenv'
+import { Client, Intents, ClientPresence, VoiceChannel } from 'discord.js'
+import { rollNumber } from './commands/roll.js'
+import { pickUser } from './commands/pick.js'
+import { coinflip } from './commands/coinflip.js'
+import { responseToJabami } from './responseToUsers/responseToJabami.js'
+dotenv.config()
 
 // client is a instance of Client class
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, "GUILD_MESSAGES", "GUILD_VOICE_STATES"]
-});
+  intents: [Intents.FLAGS.GUILDS, 'GUILD_MESSAGES', 'GUILD_VOICE_STATES']
+})
 
-const clientPresence = new ClientPresence({});
+const clientPresence = new ClientPresence({})
 
-const PREFIX = "$";
+const PREFIX = '$'
 
 // When the client is ready, run this code (only once)
-client.on("ready", () => {
-  console.log(`${client.user.username} has logged in!`);
-  client.user.setUsername("BOT ek0ng");
-});
+client.on('ready', () => {
+  console.log(`${client.user.username} has logged in!`)
+  client.user.setUsername('BOT ek0ng')
+})
 
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
-  const indexOfPhrase = Math.floor(Math.random() * 2);
-  const onePercentChange = Math.floor(Math.random() * 100);
-  // COMMANDS
+client.on('messageCreate', message => {
+  // If the author is a bot then do nothing
+  if (message.author.bot) return
+
+  // Commands
   if (message.content.startsWith(PREFIX)) {
     const [CMD_NAME, ...args] = message.content
       .trim()
       .substring(PREFIX.length)
-      .split(/\s+/);
+      .split(/\s+/)
 
-    if (CMD_NAME === "roll") {
-      rollNumber(message, args);
-    } else if (CMD_NAME === "pick") {
-      pickUser(message);
-    } else if (CMD_NAME === "flip") {
-      coinflip(message);
+    if (CMD_NAME === 'roll') {
+      rollNumber(message, args)
+    } else if (CMD_NAME === 'pick') {
+      pickUser(message)
+    } else if (CMD_NAME === 'flip') {
+      coinflip(message)
     }
   }
-});
 
-client.login(process.env.DISCORD_TOKEN);
+  if (message.author.username.includes('Vann')) {
+    responseToJabami(message)
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
---
+++ src/responseToUsers/responseToJabami.js
@@ -0,0 +1,7 @@
+function responseToJabami(message) {
+    if (Math.floor(Math.random() * 100) == 0) {
+        message.reply("gg")
+    }
+}
+
+export {responseToJabami}
